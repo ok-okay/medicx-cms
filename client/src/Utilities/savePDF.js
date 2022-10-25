6279a4d8fb2f7c8a3baab6eeebdcfd69c92cae6e
Lalit <jonsnow1762@gmail.com>
2022-10-25 21:47:14 +0530

    print-blank-check

--- client/src/Utilities/savePDF.js
+++ client/src/Utilities/savePDF.js
@@ -1,23 +1,25 @@
 import jsPDF from "jspdf";
 
 export function savePDF(patientId, prescriptionId, patientInfo, prescription) {
-  const doc = new jsPDF();
-  let text, i=2;
+  if (patientId !== undefined) {
+    const doc = new jsPDF();
+    let text, i = 2;
 
-  doc.text("Patient Id: " + patientId, 10, 10);
-  doc.text("Prescription Id: " + prescriptionId, 10, 20);
+    doc.text("Patient Id: " + patientId, 10, 10);
+    doc.text("Prescription Id: " + prescriptionId, 10, 20);
 
-  for (let key in patientInfo) {
-    text = key + ": " + patientInfo[key];
-    doc.text(text, 10, 10 + i * 10);
-    i++;
+    for (let key in patientInfo) {
+      text = key + ": " + patientInfo[key];
+      doc.text(text, 10, 10 + i * 10);
+      i++;
+    }
+
+    for (let key in prescription) {
+      text = key + ": " + prescription[key];
+      doc.text(text, 10, 10 + i * 10);
+      i++;
+    }
+
+    doc.save("prescription.pdf");
   }
-
-  for (let key in prescription) {
-    text = key + ": " + prescription[key];
-    doc.text(text, 10, 10 + i * 10);
-    i++;
-  }
-
-  doc.save("prescription.pdf");
 }
